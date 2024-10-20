8cb4f0b513b553bcee002aebb225ed100aac8045
KL_jiana <2436262516@qq.com>
2024-10-20 14:34:43 +0800

    port 3.1

--- src/main/java/melonslise/locks/client/gui/LockPickingScreen.java
+++ src/main/java/melonslise/locks/client/gui/LockPickingScreen.java
@@ -14,6 +14,7 @@ import melonslise.locks.common.network.toserver.TryPinPacket;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
@@ -113,6 +114,11 @@ public class LockPickingScreen extends AbstractContainerScreen<LockPickingContai
     public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
         this.renderBackground(pGuiGraphics);
         super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
+    }
+    // 初始化
+    @Override
+    protected void init() {
+        super.init();
     }
 
     @Override
