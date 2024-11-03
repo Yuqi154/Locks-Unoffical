7fcc5e7745227a32d48678eb55092e3ba0e01c3c
KL_jiana <2436262516@qq.com>
2024-11-03 14:58:07 +0800

    ⚙序列化问题修复（应该）

--- src/main/java/melonslise/locks/common/capability/LockableStorage.java
+++ src/main/java/melonslise/locks/common/capability/LockableStorage.java
@@ -16,64 +16,56 @@ import net.minecraftforge.common.capabilities.AutoRegisterCapability;
  * Also stores lockables which are shared by multiple chunks. Duplicate shared lockables are handled by checking if they have already been loaded before
  */
 @AutoRegisterCapability
-public class LockableStorage implements ILockableStorage
-{
-	public static final ResourceLocation ID = new ResourceLocation(Locks.ID, "lockable_storage");
+public class LockableStorage implements ILockableStorage {
+    public static final ResourceLocation ID = new ResourceLocation(Locks.ID, "lockable_storage");
 
-	public final LevelChunk chunk;
+    public final LevelChunk chunk;
 
-	public Int2ObjectMap<Lockable> lockables = new Int2ObjectLinkedOpenHashMap<Lockable>();
+    public Int2ObjectMap<Lockable> lockables = new Int2ObjectLinkedOpenHashMap<Lockable>();
 
-	public LockableStorage(LevelChunk chunk)
-	{
-		this.chunk = chunk;
-	}
+    public LockableStorage(LevelChunk chunk) {
+        this.chunk = chunk;
+    }
 
-	@Override
-	public Int2ObjectMap<Lockable> get()
-	{
-		return this.lockables;
-	}
+    @Override
+    public Int2ObjectMap<Lockable> get() {
+        return this.lockables;
+    }
 
-	@Override
-	public void add(Lockable lkb)
-	{
-		this.lockables.put(lkb.id, lkb);
-		this.chunk.setUnsaved(true);
-	}
+    @Override
+    public void add(Lockable lkb) {
+        this.lockables.put(lkb.id, lkb);
+        this.chunk.setUnsaved(true);
+    }
 
-	@Override
-	public void remove(int id)
-	{
-		this.lockables.remove(id);
-		this.chunk.setUnsaved(true);
-	}
+    @Override
+    public void remove(int id) {
+        this.lockables.remove(id);
+        this.chunk.setUnsaved(true);
+    }
 
-	@Override
-	public ListTag serializeNBT()
-	{
-		ListTag list = new ListTag();
-		for(Lockable lkb : this.lockables.values())
-			list.add(Lockable.toNbt(lkb));
-		return list;
-	}
+    @Override
+    public ListTag serializeNBT() {
+        ListTag list = new ListTag();
+        for (Lockable lkb : this.lockables.values())
+            list.add(Lockable.toNbt(lkb));
+        return list;
+    }
 
-	@Override
-	public void deserializeNBT(ListTag nbt)
-	{
-		ILockableHandler handler = this.chunk.getLevel().getCapability(LocksCapabilities.LOCKABLE_HANDLER).orElse(null);
-		Int2ObjectMap<Lockable> lkbs = handler.getLoaded();
-		for(int a = 0; a < nbt.size(); ++a)
-		{
-			CompoundTag nbt1 = nbt.getCompound(a);
-			Lockable lkb = lkbs.get(Lockable.idFromNbt(nbt1));
-			if(lkb == lkbs.defaultReturnValue())
-			{
-				lkb = Lockable.fromNbt(nbt1);
-				lkb.addObserver(handler);
-				lkbs.put(lkb.id, lkb);
-			}
-			this.lockables.put(lkb.id, lkb);
-		}
-	}
+    @Override
+    public void deserializeNBT(ListTag nbt) {
+        ILockableHandler handler = this.chunk.getLevel().getCapability(LocksCapabilities.LOCKABLE_HANDLER).orElse(null);
+        Int2ObjectMap<Lockable> lkbs = handler.getLoaded();
+        for (int a = 0; a < nbt.size(); ++a) {
+            CompoundTag nbt1 = nbt.getCompound(a);
+            Lockable lkb = lkbs.get(Lockable.idFromNbt(nbt1));
+            if (lkb == lkbs.defaultReturnValue()) {
+				if (lkb==null) return;
+                lkb = Lockable.fromNbt(nbt1);
+                lkb.addObserver(handler);
+                lkbs.put(lkb.id, lkb);
+            }
+            this.lockables.put(lkb.id, lkb);
+        }
+    }
 }
--- src/main/java/melonslise/locks/common/capability/SerializableCapabilityProvider.java
+++ src/main/java/melonslise/locks/common/capability/SerializableCapabilityProvider.java
@@ -4,23 +4,19 @@ import net.minecraft.nbt.Tag;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.INBTSerializable;
 
-public class SerializableCapabilityProvider<T extends Tag, A extends INBTSerializable<T>> extends CapabilityProvider<A> implements INBTSerializable<T>
-{
-	public SerializableCapabilityProvider(Capability<A> cap, A inst)
-	{
-		super(cap, inst);
-	}
+public class SerializableCapabilityProvider<T extends Tag, A extends INBTSerializable<T>> extends CapabilityProvider<A> implements INBTSerializable<T> {
+    public SerializableCapabilityProvider(Capability<A> cap, A inst) {
+        super(cap, inst);
+    }
 
-	@Override
-	public T serializeNBT()
-	{
+    @Override
+    public T serializeNBT() {
 
-		return this.inst.serializeNBT();
-	}
+        return this.inst.serializeNBT();
+    }
 
-	@Override
-	public void deserializeNBT(T nbt)
-	{
-		this.inst.deserializeNBT(nbt);
-	}
+    @Override
+    public void deserializeNBT(T nbt) {
+        this.inst.deserializeNBT(nbt);
+    }
 }
