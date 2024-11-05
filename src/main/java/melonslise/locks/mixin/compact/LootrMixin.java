f2988342564910705f74f91bb17e9d86b6ee3c94
KL_jiana <2436262516@qq.com>
2024-11-05 20:34:48 +0800

    🧹清理无用代码

--- src/main/java/melonslise/locks/mixin/compact/LootrMixin.java
+++ src/main/java/melonslise/locks/mixin/compact/LootrMixin.java
@@ -2,13 +2,12 @@ package melonslise.locks.mixin.compact;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import noobanidus.mods.lootr.block.entities.TileTicker;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 
-@Mixin(value = TileTicker.class)
+@Mixin(targets = "noobanidus.mods.lootr.block.entities.TileTicker")
 public class LootrMixin {
     @Redirect(method = "serverTick(Lnet/minecraftforge/event/TickEvent$ServerTickEvent;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;destroyBlock(Lnet/minecraft/core/BlockPos;Z)Z"))
     private static boolean lockLootr(ServerLevel instance, BlockPos pos, boolean b){
