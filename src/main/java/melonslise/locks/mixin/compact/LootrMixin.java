8ea6654b9711fcca3a16612bd204bef202ce70da
KL_jiana <2436262516@qq.com>
2024-11-05 20:36:26 +0800

    😌Pseudo

--- src/main/java/melonslise/locks/mixin/compact/LootrMixin.java
+++ src/main/java/melonslise/locks/mixin/compact/LootrMixin.java
@@ -3,10 +3,11 @@ package melonslise.locks.mixin.compact;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-
+@Pseudo
 @Mixin(targets = "noobanidus.mods.lootr.block.entities.TileTicker")
 public class LootrMixin {
     @Redirect(method = "serverTick(Lnet/minecraftforge/event/TickEvent$ServerTickEvent;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;destroyBlock(Lnet/minecraft/core/BlockPos;Z)Z"))
